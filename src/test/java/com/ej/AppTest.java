749c9eba885632a9e39abc970766e86b7b68c4a8
mfhoss <mfhoss@yahoo.com>
2019-07-06 08:48:55 -0400

    test

--- src/test/java/com/ej/AppTest.java
+++ src/test/java/com/ej/AppTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple java app
  */
 public class AppTest 
     extends TestCase
